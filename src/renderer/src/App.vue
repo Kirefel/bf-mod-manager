792add2a75dcdde769cbef9b6e7162d0aa0aae53
Kirefel <kirefelan@gmail.com>
2023-02-07 11:33:40 +1100

    Mockup

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,97 +1,55 @@
-<script setup>
-import Versions from './components/Versions.vue'
-</script>
-
 <template>
-  <Versions></Versions>
 
-  <svg class="hero-logo" viewBox="0 0 900 300">
-    <use xlink:href="./assets/icons.svg#electron" />
-  </svg>
-  <h2 class="hero-text">You've successfully created an Electron project with Vue</h2>
-  <p class="hero-tagline">Please try pressing <code>F12</code> to open the devTool</p>
+  <q-layout view="hHh lpR fFf">
 
-  <div class="links">
-    <div class="link-item">
-      <a target="_blank" href="https://evite.netlify.app">Documentation</a>
-    </div>
-    <div class="link-item link-dot">•</div>
-    <div class="link-item">
-      <a target="_blank" href="https://github.com/alex8088/electron-vite">Getting Help</a>
-    </div>
-    <div class="link-item link-dot">•</div>
-    <div class="link-item">
-      <a
-        target="_blank"
-        href="https://github.com/alex8088/quick-start/tree/master/packages/create-electron"
-      >
-        create-electron
-      </a>
-    </div>
-  </div>
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          Title
+        </q-toolbar-title>
+        <q-btn icon="settings" flat round />
+      </q-toolbar>
+    </q-header>
 
-  <div class="features">
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Configuring</h2>
-        <p class="detail">
-          Config with <span>electron.vite.config.js</span> and refer to the
-          <a target="_blank" href="https://evite.netlify.app/config/">config guide</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">HMR</h2>
-        <p class="detail">
-          Edit <span>src/renderer</span> files to test HMR. See
-          <a target="_blank" href="https://evite.netlify.app/guide/hmr-in-renderer.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Hot Reloading</h2>
-        <p class="detail">
-          Run <span>'electron-vite dev --watch'</span> to enable. See
-          <a target="_blank" href="https://evite.netlify.app/guide/hot-reloading.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Debugging</h2>
-        <p class="detail">
-          Check out <span>.vscode/launch.json</span>. See
-          <a target="_blank" href="https://evite.netlify.app/guide/debugging.html">docs</a>.
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Source Code Protection</h2>
-        <p class="detail">
-          Supported via built-in plugin <span>bytecodePlugin</span>. See
-          <a target="_blank" href="https://evite.netlify.app/guide/source-code-protection.html">
-            docs
-          </a>
-          .
-        </p>
-      </article>
-    </div>
-    <div class="feature-item">
-      <article>
-        <h2 class="title">Packaging</h2>
-        <p class="detail">
-          Use
-          <a target="_blank" href="https://www.electron.build">electron-builder</a>
-          and pre-configured to pack your app.
-        </p>
-      </article>
-    </div>
-  </div>
+    <q-page-container>
+      <q-list class="listclass" bordered separator>
+        <mod-item modTitle="Mod Loader" description="(Required) Enabled modding of the game" required />
+        <mod-item modTitle="Quality of Life" description="Adds many QoL and accessibility features such as screen shake reduction and more save slots" />
+        <mod-item modTitle="Enhanced Debug" description="Adds more debug features" />
+        <mod-item modTitle="Rando" description="You know what this is" />
+        <mod-item modTitle="Scene Explorer" description="A utility for exploring the Unity objects and components" />
+        <mod-item modTitle="Speedrun.com" description="Replaces the in-game leaderboards with ones sourced from speedrun.com" />
+      </q-list>
+    </q-page-container>
+
+    <q-footer elevated class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-space />
+        <q-btn color="green">
+          Launch
+        </q-btn>
+        <q-space />
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+
 </template>
 
-<style lang="less">
-@import './assets/css/styles.less';
+<script>
+import ModItem from './components/ModItem.vue'
+
+export default {
+  components: {
+    ModItem
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+.listclass {
+
+}
+
 </style>
---
+++ src/renderer/src/components/ModItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-item>
+    <q-item-section side top>
+      <q-checkbox disable="{{ required }}" />
+    </q-item-section>
+    <q-item-section>
+      <q-item-label>{{ modTitle }}</q-item-label>
+      <q-item-label caption lines="2"> {{ description }}</q-item-label>
+    </q-item-section>
+    <q-item-section side>
+      <q-btn-group flat rounded>
+        <q-btn icon="file_download" title="Download" />
+        <q-btn icon="description" title="View Readme" />
+      </q-btn-group>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'mod-item',
+
+  props: {
+    modTitle: String,
+    description: String,
+    required: Boolean
+  }
+}
+</script>
