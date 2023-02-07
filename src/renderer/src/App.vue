f51f75ae81b4eb87dd3c76ab49bf06e95311b293
Kirefel <kirefelan@gmail.com>
2023-02-07 12:03:40 +1100

    Settings popup

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -5,18 +5,39 @@
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-toolbar-title>
-          Title
+          Ori Launcher
         </q-toolbar-title>
-        <q-btn icon="settings" flat round />
+        <q-btn icon="settings" flat round @click="settingsVisible = true" />
       </q-toolbar>
     </q-header>
 
+    <q-dialog v-model="settingsVisible">
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section class="row items-center q-pb-none">
+          <div class="text-h6">Settings</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+
+        <q-card-section>
+          <q-checkbox>Use Steam</q-checkbox>
+
+            <q-input label="Game path" spellcheck="false">
+              <template v-slot:append>
+                <q-btn flat>...</q-btn>
+              </template>
+            </q-input>
+          <q-checkbox>Debug mode</q-checkbox>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+
     <q-page-container>
       <q-list class="listclass" bordered separator>
-        <mod-item modTitle="Mod Loader" description="(Required) Enabled modding of the game" required />
-        <mod-item modTitle="Quality of Life" description="Adds many QoL and accessibility features such as screen shake reduction and more save slots" />
+        <mod-item modTitle="Mod Loader" description="(Required) Enabled modding of the game" required installed enabled />
+        <mod-item modTitle="Quality of Life" description="Adds many QoL and accessibility features such as screen shake reduction and more save slots" installed />
         <mod-item modTitle="Enhanced Debug" description="Adds more debug features" />
-        <mod-item modTitle="Rando" description="You know what this is" />
+        <mod-item modTitle="Rando" description="You know what this is" installed enabled />
         <mod-item modTitle="Scene Explorer" description="A utility for exploring the Unity objects and components" />
         <mod-item modTitle="Speedrun.com" description="Replaces the in-game leaderboards with ones sourced from speedrun.com" />
       </q-list>
@@ -38,10 +59,17 @@
 
 <script>
 import ModItem from './components/ModItem.vue'
+import { ref } from 'vue'
 
 export default {
   components: {
     ModItem
+  },
+
+  setup() {
+    return {
+      settingsVisible: ref(false)
+    }
   }
 }
 </script>
--- src/renderer/src/components/ModItem.vue
+++ src/renderer/src/components/ModItem.vue
@@ -1,29 +1,32 @@
 <template>
   <q-item>
     <q-item-section side top>
-      <q-checkbox disable="{{ required }}" />
+      <q-checkbox :disable="required" v-model="state.enabled" />
     </q-item-section>
     <q-item-section>
       <q-item-label>{{ modTitle }}</q-item-label>
       <q-item-label caption lines="2"> {{ description }}</q-item-label>
     </q-item-section>
     <q-item-section side>
-      <q-btn-group flat rounded>
+      <q-btn-group flat>
         <q-btn icon="file_download" title="Download" />
-        <q-btn icon="description" title="View Readme" />
+        <q-btn icon="launch" title="View in Browser" />
       </q-btn-group>
     </q-item-section>
   </q-item>
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'mod-item',
 
   props: {
     modTitle: String,
     description: String,
-    required: Boolean
+    required: Boolean,
+    state
   }
 }
 </script>
